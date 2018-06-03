b380672ff9e2060a7d29ce98342ef23ef9612484
Issouf zabre <sheridanzabre@gmail.com>
2018-06-03 06:38:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(kitten){
+  kittens.push(kitten)
+  return kittens
+}
+
+function destructivelyPrependKitten(kitten){
+  kittens.unshift(kitten)
+  return kittens
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+  return kittens
+}
+
+function prependKitten(kitten){
+  kittens.shift()
+  return kittens
+}
+
+function appendKitten(kitten){
+  return kittens.slice(1)
+}
